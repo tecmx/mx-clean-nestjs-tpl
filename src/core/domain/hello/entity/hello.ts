8885bf578afa55921cc13c402de76dfe3e87bd19
Miguel Xavier <1868619+miguelxvr@users.noreply.github.com>
2023-05-04 20:41:59 +0000

    initial commit

--- src/core/domain/hello/entity/hello.ts
+++ src/core/domain/hello/entity/hello.ts
@@ -1,8 +1,9 @@
-import { IsDate, IsOptional, IsString } from 'class-validator';
+import { IsDate, IsOptional, IsString } from "class-validator";
 import { Entity } from "../../../common/entity/Entity";
 import { RemovableEntity } from "../../../common/entity/RemovableEntity";
+import { Nullable } from "../../../common/type/CommonType";
 
-export class Hello extends Entity<string> implements RemovableEntity
+export class Hello extends Entity<string> implements RemovableEntity {
   @IsString()
   private _intAttr: number;
 
@@ -11,11 +12,11 @@ export class Hello extends Entity<string> implements RemovableEntity
 
   @IsDate()
   private readonly createdAt: Date;
-  
+
   @IsOptional()
   @IsDate()
   private editedAt: Nullable<Date>;
-  
+
   @IsOptional()
   @IsDate()
   private removedAt: Nullable<Date>;
@@ -32,5 +33,9 @@ export class Hello extends Entity<string> implements RemovableEntity
 
   get stringAttr() {
     return this._stringAttr;
+  }
+
+  remove(): Promise<void> {
+    throw new Error("Method not implemented.");
   }
 }
